8e85b5d3c1ac16d739c9a08b2d645fd13fefc7e0
srimathiradhakrishnan <srimathiradhakrishna@gmail.com>
2023-03-24 10:35:24 -0400

    Completed lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // code your solution here
+function saturdayFun(play = "roller-skate"){
+    return (`This Saturday, I want to ${play}!`);
+}
+console.log(saturdayFun());
+console.log(saturdayFun('bathe my dog'));
+function mondayWork(park = `go to the office`){
+    return (`This Monday, I will ${park}.`)
+}
+console.log(mondayWork());
+console.log(mondayWork(`work from home`));
+
+function wrapAdjective(special , msg = 'You are'){
+    return function (txt){
+        return `${msg} ${special}${txt}${special}!`;
+    };
+    }
+   console.log (wrapAdjective('*')("a hard worker"));
+   console.log(wrapAdjective("%")("a dedicated programmer"));
+
